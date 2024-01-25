beed5bc97c468021926ec82c9a55417b59d68803
MOHIT-IITP <codingbymohit@gmail.com>
2024-01-25 11:14:44 +0000

    add print_n_to_1_using_recursion

---
+++ Recursion/print_n_to_1_using_recursion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+void fun1(int n)
+{
+    if(n>0)
+    {
+        cout<<n<<endl;
+        fun1(n-1);
+    }
+}
+
+int main()
+{
+    int x;
+    cin>>x;
+    cout<<"The Answer is ";
+    fun1(x);
+    return 0;
+}
