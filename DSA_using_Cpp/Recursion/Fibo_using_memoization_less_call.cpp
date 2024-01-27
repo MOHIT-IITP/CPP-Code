44d9de1fe9c58b111495a3f39ae32c46e5ba6683
MOHIT-IITP <codingbymohit@gmail.com>
2024-01-27 16:04:26 +0000

    Add fibo using memoization less call

---
+++ DSA_using_Cpp/Recursion/Fibo_using_memoization_less_call.cpp
@@ -0,0 +1,36 @@
+// using process called memoization
+#include<iostream>
+
+using namespace std;
+
+int f[10];
+
+int fibo(int n)
+{
+	if(n<=1)
+	{
+		f[n]=n;
+		return n;
+
+	}
+	else
+	{
+		if(f[n-1]==-1)
+			f[n-1]=fibo(n-1);
+		if(f[n-2]==-1)
+			f[n-2]=fibo(n-2);
+		return f[n-2]+f[n-1];
+	}
+}
+
+int main()
+{
+	int i;
+	int x;
+	cin>>x;
+	for(i=0;i<10;++i)
+		f[i]=-1;
+	int result = fibo(x);
+	cout<<result;
+	return 0;
+}
