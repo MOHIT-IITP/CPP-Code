420d405a2ce8e6bee14092b2d84acef1636ff4a2
MOHIT-IITP <codingbymohit@gmail.com>
2024-01-27 16:41:56 +0000

    Add new Tower of hanoi easy version

---
+++ DSA_using_Cpp/Recursion/TowerOfHanoi_Easy_method.cpp
@@ -0,0 +1,29 @@
+// Easy method to learn/ understand the tower of Hanoi problem 
+
+// TOWER OF HANOI 
+
+
+#include<iostream>
+
+using namespace std;
+
+// main code is here 
+
+void toh(int n,int A , int B , int C)
+{
+	if(n>0)
+	{
+		toh(n-1,A,C,B);
+		cout<<"Move "<<A<<" to "<<C<<endl;
+		toh(n-1,B,A,C);
+	}
+}
+
+int main()
+{
+	int m,j,k,l;
+	cin>>m>>j>>k>>l;
+	cout<<"The Answer is "<<endl;
+	toh(m,j,k,l);
+	return 0;
+}
