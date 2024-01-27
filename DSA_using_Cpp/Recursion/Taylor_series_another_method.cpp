bc9b9ff9a101ff1dea5018d22bb0f35d18caf531
MOHIT-IITP <codingbymohit@gmail.com>
2024-01-27 17:18:21 +0000

    Update taylor series

---
+++ DSA_using_Cpp/Recursion/Taylor_series_another_method.cpp
@@ -0,0 +1,30 @@
+// TAYLOR SERIES USING ANOTHER METHOD
+
+//MOHIT-IITP
+
+#include<iostream>
+
+using namespace std;
+
+double taylor(int x, int n)
+{
+    static double p=1,f=1;
+    double r;
+    if(n==0)
+    {
+        return 1;
+    }
+    r=taylor(x,n-1);
+    p=p*x;
+    f=f*n;
+    return r+p/f;
+}
+
+int main()
+{
+    double j,k;
+    cin>>j>>k;
+    cout<<"Your Answer is :";
+    cout<<taylor(j,k);
+    return 0;
+}
