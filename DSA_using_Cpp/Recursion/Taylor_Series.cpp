0489878a732703c053d2fc84b18f32df11ffc6f5
MOHIT-IITP <codingbymohit@gmail.com>
2024-01-27 15:50:50 +0000

    Add Dsa folder

---
+++ DSA_using_Cpp/Recursion/Taylor_Series.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+double e(int x,int n)
+{
+  static double s=1;
+  if(n==0)
+    return s;
+  s=1+x*s/n;
+  return e(x,n-1);
+}
+
+int main()
+{
+  double j,k;
+  cin>>j>>k;
+  cout<<"The Answer is ";
+  cout<<e(j,k);
+	return 0;
+}
