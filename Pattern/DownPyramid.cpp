9d5536586d4434c5f6a77e81e35a38a456acfe8e
MOHIT-IITP <codingbymohit@gmail.com>
2024-02-06 17:51:18 +0000

    Add pyramid

---
+++ Pattern/DownPyramid.cpp
@@ -0,0 +1,37 @@
+/* The output will be something like this  :
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+*/
+#include<iostream>
+using namespace std;
+
+ 
+void pattern(int n)
+{
+	int row,col;
+    for(row=n;row>0;row--)
+    {
+    	for(col=1;col<=n-row;col++)
+    	{
+    		cout<<" ";
+    	}
+    	for(col=1;col<=2*row-1;col++)
+    	{
+    		cout<<"*";
+    	}
+    	cout<<endl;
+    }
+}
+
+
+int main()
+{
+	int m;
+	cin>>m;
+	pattern(m);
+	return 0;
+}
