8aa124195503a93290ed94b7e7389faad4f9b6e1
MOHIT-IITP <codingbymohit@gmail.com>
2024-02-06 17:44:18 +0000

    Add updownpyramid

---
+++ Pattern/upAndDownPyramid.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n)
+{
+	int row,col;
+    for(row=n;row>0;row--)
+    {
+    	for(col=1;col<=n-row;col++)
+    	{
+    		cout<<" ";
+    	}
+    	for(col=1;col<=2*row-1;col++)
+    	{
+    		cout<<"*";
+    	}
+    	cout<<endl;
+    }
+	for(row=1;row<=n; row++)
+	{
+		for(col=1;col<=n-row;col++)
+		{
+			cout<<" ";
+		}
+		for(col=1;col<=2*row-1;col++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+    }
+
+}
+
+
+int main()
+{
+	int m;
+	cin>>m;
+	pattern(m);
+	return 0;
+}
