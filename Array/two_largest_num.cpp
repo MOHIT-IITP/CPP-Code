3ee08d0cb176cd28600437481f85027013d22de0
MOHIT-IITP <codingbymohit@gmail.com>
2024-02-16 16:57:00 +0000

    Add array code

---
+++ Array/two_largest_num.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void two_largest(int a[])
+{
+	int second=0;
+	int first=0;
+	for (int i=0;i<5;i++)
+	{
+		if(a[i]>first)
+		{
+			first=a[i];
+		}
+		else if(a[i]>second && second !=first)
+		{
+			second = a[i];
+		}
+	}
+	cout<<first<<endl;
+	cout<<second;
+}
+
+int main()
+{
+	int a[5]={91,22,43,44,35};
+	two_largest(a);
+	return 0;
+}
