86848660e9f7a430ded14f2ec9dce08effdde673
MOHIT-IITP <codingbymohit@gmail.com>
2024-02-16 17:39:23 +0000

    Add new array codee

---
+++ Array/first_three_large.cpp
@@ -0,0 +1,35 @@
+void larg_three(int a[])
+{
+	int first,second,third;
+	first=0;
+	second=0;
+	third=0;
+	for(int i =0;i<5;++i)
+	{
+		if(first<a[i])
+		{
+			third=second;
+			second = first;
+			first=a[i];
+		}
+		else if(a[i]>second && a[i]!=first)
+		{
+			third=second;
+			second=a[i];
+		}
+		else if(a[i]>third && a[i]!=second && a[i]!=first)
+		{
+			third=a[i];
+		}
+	}
+	cout<<"First :"<<first<<endl;
+	cout<<"Second : "<<second<<endl;
+	cout<<"Third : "<<third<<endl;
+}
+
+int main()
+{
+	int a[5]={34,23,43,23,54};
+	larg_three(a);
+	return 0;
+}
