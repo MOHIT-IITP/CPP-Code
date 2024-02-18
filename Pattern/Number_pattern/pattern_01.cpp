8c68272d5e9d9ca574c804c6cf29cb1a595261bb
MOHIT-IITP <codingbymohit@gmail.com>
2024-02-18 12:27:16 +0530

    Add number pattern

--- Pattern/Number_pattern/pattern_01.cpp
+++ Pattern/Number_pattern/pattern_01.cpp
@@ -2,11 +2,10 @@
 // output will be something like this 
 
 // 1
-// 23
-// 456
-// 78910
-// 1112131415
-
+// 12
+// 123
+// 1234
+// 12345
 
 
 #include<iostream>
@@ -15,9 +14,9 @@ using namespace std;
 
 void pattern(int row)
 {
-	int num=1;
 	for(int i =0;i<row;i++)
 	{
+	    int num=1;
 		for(int col =0;col<i;++col)
 		{
 			cout<<num;
